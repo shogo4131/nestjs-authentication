fe28f632cf0af12c33c2cbdc2abbe0cd40d4ef2f
shogo4131 <shougo08697118@gmail.com>
2022-04-24 15:42:43 +0900

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +16,22 @@ async function bootstrap() {
     origin: process.env.FRONTEND_URL,
   });
 
+  /* class-validationをグルーバルに使用する設定 */
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Authorizations API')
+    .setDescription('NestJSを用いたAPI')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  fs.writeFileSync(
+    './swagger/swagger-spec.json',
+    JSON.stringify(document, undefined, 2),
+  );
 
   /* PORT設定 */
   await app.listen(process.env.PORT || 3000);
